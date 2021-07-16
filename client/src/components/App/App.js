803b70e578479a7e6ac9b5dce3a6b5f487763a25
Aleksey <mobb.steam@gmail.com>
2021-07-16 14:12:30 +0300

    Fix nav and profile

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,13 +1,9 @@
-
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import LoginForm from '../LoginForm/LoginForm'
 import Main from '../Main/Main'
 import Nav from '../Nav/Nav'
 import Profile from '../Profile/Profile'
 import SignUpForm from '../SignUpForm/SignUpForm'
-import PetCard from '../PetCard/PetCard'
 import LoginForm from '../LoginForm/LoginForm'
-import SignUpForm from '../SignUpForm/SignUpForm'
 import './App.css'
 import { createBrowserHistory } from 'history'
 
--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import PetCard from '../PetCard/PetCard'
 
 function Profile() {
   return (
     <>
-      
+      <PetCard/>
     </>
   )
 }
