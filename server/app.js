ec89cc3877ab76b490a8fb2c407b14d32559ea4a
Treister Aleksei <45953646+TipTipple@users.noreply.github.com>
2021-07-16 11:21:03 +0300

    create sceleton auth

--- server/app.js
+++ server/app.js
@@ -3,15 +3,19 @@ const path = require('path')
 const connect = require('./db/connect')
 const dotenv = require('dotenv')
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 
+const routerAuth = require('./routers/index.js')
 const indexRouter = require('./routers/indexRouter')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 app.use(cors())
 
+app.use('/api', routerAuth)
 app.use('/', indexRouter)
 
 module.exports = app
---
+++ server/db/models/user.midel.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema({
+  name: { type: String, require: true },
+  email: { type: String, unique: true, require: true },
+  password: { type: String, require: true },
+  isActivated: { type: Boolean, default: false },
+  activationLink: { type: String }, // link to activated
+  pets: [{ type: Schema.Types.ObjectId, ref: 'Pet' }],
+})
+
+// const User = model('User', userSchema)
+
+module.exports = model('User', userSchema)
