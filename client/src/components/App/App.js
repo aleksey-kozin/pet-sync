cf68d6fc7969a7bc3b953c75e4150d0aa79b6e06
Aleksey <mobb.steam@gmail.com>
2021-07-16 13:57:03 +0300

    Add navigation

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,14 +1,25 @@
-import LoginForm from '../LoginForm/LoginForm';
-import SignUpForm from '../SignUpForm/SignUpForm';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import LoginForm from '../LoginForm/LoginForm'
+import Main from '../Main/Main'
+import Nav from '../Nav/Nav'
+import Profile from '../Profile/Profile'
+import SignUpForm from '../SignUpForm/SignUpForm'
+import './App.css'
 
 function App() {
   return (
     <>
-      <LoginForm />
-      {/* <SignUpForm /> */}
+      <Router>
+        <Nav />
+        <Switch>
+          <Route path={'/'} exact component={Main} />
+          <Route path={'/login'} component={LoginForm} />
+          <Route path={'/signup'} component={SignUpForm} />
+          <Route path={'/profile'} component={Profile} />
+        </Switch>
+      </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
