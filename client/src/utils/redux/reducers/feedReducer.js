58382a4e8ec477453e18139c512fb3383814d54f
Katerina <ekaterinalf@mail.ru>
2021-07-20 15:09:12 +0300

    edit feed db

--- client/src/utils/redux/reducers/feedReducer.js
+++ client/src/utils/redux/reducers/feedReducer.js
@@ -1,9 +1,17 @@
-import { INIT_FEED } from '../actionTypes/actionTypes'
+import { INIT_FEED, EDIT_FEED } from "../actionTypes/actionTypes";
 
 const feedReducer = (state = {feed: []}, action) => {
 switch (action.type) {
   case INIT_FEED:
     return { ...state, feed: action.payload };
+  case EDIT_FEED:
+    const index = state.feed.findIndex(
+      (el) => el._id === action.payload._id);
+      const newArray = [...state.feed];
+      newArray[index] = {
+        ...action.payload
+      };
+      return { ...state, feed: newArray };
   default:
     return state;
 }
