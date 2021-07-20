48d7891ce0ea4bfac8d3e53ca951414498b032ae
Treister Aleksei <45953646+TipTipple@users.noreply.github.com>
2021-07-20 15:08:36 +0300

    Fix bag

--- client/src/components/PetPersonPage/PetPersonPage.js
+++ client/src/components/PetPersonPage/PetPersonPage.js
@@ -9,21 +9,21 @@ function PetPersonPage(props) {
   const { id } = useParams()
   const petState = useSelector((state) => state.petsReducer.pet)
   const pet = petState.find((el) => el._id === id)
-  console.log('pet',pet)
+  console.log('pet', pet._id)
   return (
     <>
       <div className="container">
         <div className="main-wrapper1">
           <ProfileNav />
           <div className="pet-profile">
-          <h4>Кличка: {pet.name ? pet.name : 'нет данных'}</h4>
-          <p>Вид: {pet.spacies ? pet.spacies : 'нет данных'}</p>
-          <p>Порода: {pet.breed ? pet.breed : 'нет данных'}</p>
-          <p>Пол: {pet.sex ? pet.sex : 'нет данных'}</p>
-          <p>Вес: {pet.weight ? pet.weight : 'нет данных'}</p>
-          <p>Дата рождения: {pet.birthdate ? pet.birthdate : 'нет данных'}</p>
+            <h4>Кличка: {pet.name ? pet.name : 'нет данных'}</h4>
+            <p>Вид: {pet.spacies ? pet.spacies : 'нет данных'}</p>
+            <p>Порода: {pet.breed ? pet.breed : 'нет данных'}</p>
+            <p>Пол: {pet.sex ? pet.sex : 'нет данных'}</p>
+            <p>Вес: {pet.weight ? pet.weight : 'нет данных'}</p>
+            <p>Дата рождения: {pet.birthdate ? pet.birthdate : 'нет данных'}</p>
           </div>
-           <ChartList />
+          <ChartList petId = {pet._id} />
         </div>
       </div>
     </>
